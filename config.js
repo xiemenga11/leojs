6634688628fbad1f538ad2f41fb346fb65f073f4
leoxie <xiemenga11@126.com>
2019-01-30 00:06:36 +0800

    auto:index.html is modified[2019-01-30 00:06:16]

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = [
+	{
+		name:'背景连线',
+		url:'beijinglianxian.html'
+	},
+	{
+		name:'优酷视频外链转换器',
+		url:'youkuVideoUrlParser.html'
+	},
+	{
+		name:'飞机大战',
+		url:'plane/index.html'
+	},{
+		name:'飞翔小鸟',
+		url:'flappyBird/index.html'
+	}
+]
